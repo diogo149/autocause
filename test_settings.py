9d736ed4d4d0041e8c7d2115bf1f8cdd41e35482
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-01-07 22:08:16 -0800

    allow test_settings to take in a file name for a custom config

--- test_settings.py
+++ test_settings.py
@@ -1,4 +1,6 @@
 #!/bin/env python
+import sys
+
 from autocause.challenge import read_all
 from autocause.core import featurize
 
@@ -8,5 +10,6 @@ if __name__ == "__main__":
     pairs = read_all("sample")[0]
     new_pairs = [(a[:SAMPLE_SIZE], a_type, b[:SAMPLE_SIZE], b_type)
                  for a, a_type, b, b_type in pairs]
-    f = featurize(new_pairs)
+    config = None if len(sys.argv) < 2 else sys.argv[1]
+    f = featurize(new_pairs, config)
     print(f.shape)
